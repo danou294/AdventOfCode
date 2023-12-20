2902bdb0f65ca685d027376d21e5025acd2f1110
Daniel LEVY <d.levy@socoda.com>
2023-12-20 15:37:32 +0100

    day7/part1

--- Day7/part1/script.py
+++ Day7/part1/script.py
@@ -1,75 +1,116 @@
-def compare_hands(hand1, hand2):
-    """
-    Compare two hands based on their type and individual card strengths.
-    """
-    card_values = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2}
-    
-    def get_hand_type(hand):
-        """
-        Determine the type of the hand.
-        """
-        counts = {label: hand.count(label) for label in set(hand)}
-        count_values = sorted(counts.values(), reverse=True)
-
-        if count_values == [5]:
-            return "Cinq d'une sorte"
-        elif count_values == [4, 1]:
-            return "Carré"
-        elif count_values == [3, 2]:
-            return "Full house"
-        elif count_values == [3, 1, 1]:
-            return "Brelan"
-        elif count_values == [2, 2, 1]:
-            return "Deux paires"
-        elif count_values == [2, 1, 1, 1]:
-            return "Une paire"
+def evaluer_main(main):
+    frequences = {}
+    for carte in main:
+        if carte in frequences:
+            frequences[carte] += 1
         else:
-            return "Carte haute"
-    
-    hand1_type = get_hand_type(hand1)
-    hand2_type = get_hand_type(hand2)
-    types = ["Cinq d'une sorte", "Carré", "Full house", "Brelan", "Deux paires", "Une paire", "Carte haute"]
+            frequences[carte] = 1
 
-    # Compare types
-    if types.index(hand1_type) < types.index(hand2_type):
-        return -1  # hand1 est plus faible
-    elif types.index(hand1_type) > types.index(hand2_type):
-        return 1  # hand1 est plus fort
+    tri_frequences = sorted(frequences.values(), reverse=True)
+
+    if tri_frequences[0] == 5:
+        return "Cinq d'une sorte", tri_frequences
+    elif tri_frequences[0] == 4:
+        return "Carré", tri_frequences
+    elif tri_frequences[0] == 3:
+        if tri_frequences[1] == 2:
+            return "Full House", tri_frequences
+        else:
+            return "Brelan", tri_frequences
+    elif tri_frequences[0] == 2:
+        if tri_frequences[1] == 2:
+            return "Deux Paires", tri_frequences
+        else:
+            return "Une Paire", tri_frequences
     else:
-        # If types are the same, compare individual cards
-        for card1, card2 in zip(hand1, hand2):
-            if card_values[card1] > card_values[card2]:
-                return 1  # hand1 est plus fort
-            elif card_values[card1] < card_values[card2]:
-                return -1  # hand1 est plus faible
-        # Si toutes les cartes sont égales, c'est une égalité
-        return 0
+        return "Carte Haute", tri_frequences
 
-def rank_hands(entries):
+def trier_par_type(mains):
+    mains_par_type = {
+        "Cinq d'une sorte": [],
+        "Carré": [],
+        "Full House": [],
+        "Brelan": [],
+        "Deux Paires": [],
+        "Une Paire": [],
+        "Carte Haute": []
+    }
+
+    for main in mains:
+        type_main, _ = evaluer_main(main)
+        mains_par_type[type_main].append(main)
+
+    return mains_par_type
+
+def comparer_cartes(main1, main2, valeur_cartes):
+    for i in range(5):
+        if valeur_cartes[main1[i]] > valeur_cartes[main2[i]]:
+            return True
+        elif valeur_cartes[main1[i]] < valeur_cartes[main2[i]]:
+            return False
+    return False  # Égalité
+
+def classer_mains(mains):
+    valeur_cartes = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    mains_par_type = trier_par_type(mains)
+    mains_classees = []
+
+    for type_main in ["Cinq d'une sorte", "Carré", "Full House", "Brelan", "Deux Paires", "Une Paire", "Carte Haute"]:
+        mains_type = mains_par_type[type_main]
+        mains_type.sort(key=lambda main: [valeur_cartes[carte] for carte in main], reverse=True)
+        mains_classees.extend(mains_type)
+
+    return mains_classees
+
+# Adapter l'algorithme pour lire les mains et les enchères à partir du nouvel input
+
+def lire_input_et_extraire_mains_avec_encheres(filepath):
     """
-    Rank the hands based on their strength.
+    Lit le fichier d'input et extrait les mains et les enchères.
+
+    :param filepath: Chemin du fichier contenant les mains et les enchères.
+    :return: Liste de tuples contenant les mains et les enchères associées.
     """
-    hands_and_bids = [(entry.split()[0], int(entry.split()[1])) for entry in entries]
+    mains_et_encheres = []
+    with open(filepath, 'r') as file:
+        for ligne in file:
+            # Séparer la main et l'enchère
+            partie_main, enchere = ligne.strip().split()
+            mains_et_encheres.append((partie_main, int(enchere)))
+    return mains_et_encheres
 
-    # Sort hands by their strength (first by type, then by individual card strength)
-    sorted_hands = sorted(hands_and_bids, key=lambda hand_bid: (
-        compare_hands(hand_bid[0], ""),
-        compare_hands(hand_bid[0], hand_bid[0])
-    ), reverse=True)
+def classer_mains_avec_encheres(mains_et_encheres):
+    # Extraire uniquement les mains pour le classement
+    mains_seules = [main for main, enchere in mains_et_encheres]
+    # Classer les mains
+    mains_classees = classer_mains(mains_seules)
 
-    # Assign ranks to the sorted hands
-    ranked_hands = [(hand_bid[0], rank, hand_bid[1]) for rank, hand_bid in enumerate(sorted_hands, start=1)]
-    return ranked_hands
+    # Calculer le nombre total de mains
+    nombre_mains = len(mains_classees)
 
-# Exemple d'entrée
-example_entries = [
-    "32T3K 765",
-    "T55J5 684",
-    "KK677 28",
-    "KTJJT 220",
-    "QQQJA 483"
-]
+    # Associer chaque main classée à son enchère et calculer le gain
+    resultats = []
+    for i, main_classee in enumerate(mains_classees):
+        rang = nombre_mains - i  # Le rang 1 est attribué à la main la plus faible
+        enchere = next(enchere for main, enchere in mains_et_encheres if main == main_classee)
+        gain = enchere * rang
+        resultats.append((main_classee, rang, gain))
 
-# Obtenir la liste combinée
-combined_list = rank_hands(example_entries)
-print(combined_list)
+    return resultats
+
+# Lire les mains et les enchères à partir du fichier input
+mains_et_encheres = lire_input_et_extraire_mains_avec_encheres('input.txt')
+
+# Exécution du classement avec les enchères
+resultats = classer_mains_avec_encheres(mains_et_encheres)
+
+# Calculer le gain total
+gain_total = sum(gain for _, _, gain in resultats)
+
+
+
+# Affichage des mains classées avec rang et gain
+for main, rang, gain in resultats:
+    print(f"Main: {main}, Rang: {rang}, Gain: {gain}")
+print(f"Gain total: {gain_total}")
+

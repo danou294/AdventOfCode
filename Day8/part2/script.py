c52485fd131f8f57a1b55f9d93654bb0592005cd
Daniel LEVY <d.levy@socoda.com>
2023-12-21 12:05:59 +0100

    day8-part2

--- Day8/part2/script.py
+++ Day8/part2/script.py
@@ -1,83 +1,50 @@
-class Node:
-    def __init__(self, value, left, right):
-        self.value = value
-        self.left = left
-        self.right = right
+import sys
+import re
+from math import gcd
+from collections import defaultdict, Counter
 
-def parse_input(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    return lines
+# Lecture du fichier 'input.txt'
+with open('input.txt') as file:
+    D = file.read().strip()
+L = D.split('\n')
 
-def get_total_part_one(input_data):
-    instructions = input_data[0].strip()
-    nodes = {}
-    for line in input_data[1:]:
-        parts = line.strip().split(' = ')
-        if len(parts) != 2:
-            continue
-        value = parts[0]
-        left, right = parts[1].strip('()').split(', ')
-        nodes[value] = Node(value, left, right)
+def lcm(xs):
+    ans = 1
+    for x in xs:
+        ans = (x*ans)//gcd(x, ans)
+    return ans
 
-    cur_node = nodes["AAA"]
-    cursor = 0
-    count = 1
+GO = {'L': {}, 'R': {}}
+steps, rule = D.split('\n\n')
+for line in rule.split('\n'):
+    st, lr = line.split('=')
+    st = st.strip()
+    left, right = lr.split(',')
+    left = left.strip()[1:].strip()
+    right = right[:-1].strip()
+    GO['L'][st] = left
+    GO['R'][st] = right
+
+def solve(part2):
+    POS = []
+    for s in GO['L']:
+        if s.endswith('A' if part2 else 'AAA'):
+            POS.append(s)
+    T = {}
+    t = 0
     while True:
-        instruction = instructions[cursor]
-        next_node = cur_node.right if instruction == 'R' else cur_node.left
-        if next_node == "ZZZ":
-            return count
-        cur_node = nodes[next_node]
-        count += 1
-        cursor = (cursor + 1) % len(instructions)
+        NP = []
+        for i, p in enumerate(POS):
+            p = GO[steps[t%len(steps)]][p]
+            if p.endswith('Z'):
+                T[i] = t+1
+                if len(T) == len(POS):
+                    return lcm(T.values())
+            NP.append(p)
+        POS = NP
+        t += 1
+    assert False
 
-def gcd(a, b):
-    while a:
-        a, b = b % a, a
-    return b
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-def get_total_part_two(input_data):
-    instructions = input_data[0].strip()
-    nodes = {}
-    for line in input_data[1:]:
-        parts = line.strip().split(' = ')
-        if len(parts) != 2:
-            continue
-        value = parts[0]
-        left, right = parts[1].strip('()').split(', ')
-        nodes[value] = Node(value, left, right)
-
-    cursor = 0
-    count = 0
-    concurrent_paths = [nodes[key] for key in nodes if key.endswith('A')]
-    loop_lengths = []
-
-    while True:
-        if len(loop_lengths) == len(concurrent_paths):
-            result = lcm(loop_lengths[0], loop_lengths[1])
-            for length in loop_lengths[2:]:
-                result = lcm(result, length)
-            return result
-
-        instruction = instructions[cursor]
-        for i, cur_node in enumerate(concurrent_paths):
-            next_node = cur_node.right if instruction == 'R' else cur_node.left
-            if next_node.endswith('Z'):
-                loop_lengths.append(count + 1)
-                concurrent_paths[i] = None
-            else:
-                concurrent_paths[i] = nodes[next_node]
-
-        concurrent_paths = [node for node in concurrent_paths if node is not None]
-        count += 1
-        cursor = (cursor + 1) % len(instructions)
-
-
-# Adapted usage
-input_data = parse_input('input.txt')
-print("Total Part One:", get_total_part_one(input_data))
-print("Total Part Two:", get_total_part_two(input_data))
+# Exécution de l'algorithme avec les deux variantes
+print(solve(False))
+print(solve(True))
